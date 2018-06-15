413001fd0b76641aa073842e794cafd17332b1b8
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-06-15 03:23:19 +0000

    Done.

--- dice_roll.rb
+++ dice_roll.rb
@@ -4,4 +4,11 @@
 
 def roll
   # code goes here
+  rand(1..6)
 end
+
+def roll_array
+  array_of_numbers = [1, 2, 3, 4, 5, 6]
+  array_of_numbers[rand(array_of_numbers.length)]
+  binding.pry
+end  
